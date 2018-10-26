37bbc1da8c71fa6efa81beb41d29941bce062f5a
BlueGlowstick <jflewis91@gmail.com>
2018-10-26 15:15:41 +0000

    Fix typo in draw function.

--- breakoutgame.js
+++ breakoutgame.js
@@ -2,8 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
-
-function () {
+function draw () {
     ctx.beginPath();
     ctx.arc(50, 50, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
