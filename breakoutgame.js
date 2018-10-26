38823ca26624bcc58b4f90614e3e39a97f28379d
BlueGlowstick <jflewis91@gmail.com>
2018-10-26 17:06:33 +0000

    Add basic collision detection code.

--- breakoutgame.js
+++ breakoutgame.js
@@ -6,10 +6,11 @@ var y = canvas.height-30;
 // Ball speed set by dx, dy and interval rate
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -20,6 +21,12 @@ function draw() {
     drawBall();
     x += dx;
     y += dy;
+    if(x + dx > canvas.width || x + dx < 0) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height || y + dy < 0) {
+        dy = -dy;
+    }
 }
 
 setInterval(draw, 10);
